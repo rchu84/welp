55ecc82f8c872cefceaf2d6b8246862950b6e592
Richard Chu <rchu84@gmail.com>
2020-03-02 00:44:08 -0800

    added photos carousel to list view; changed map icon and mouseEnter/mouseLeave icons

--- frontend/src/components/biz/biz_list.jsx
+++ frontend/src/components/biz/biz_list.jsx
@@ -5,11 +5,17 @@ import { Link as RouterLink } from "react-router-dom";
 
 import queryString from "query-string";
 import {
-  Grid, Card, CardContent, Box, Link, CardActionArea
+  Grid, Card, CardContent, Box, Link, CardActionArea, CardMedia
 } from '@material-ui/core';
 import {
   Rating, Pagination
 } from "@material-ui/lab";
+
+import Carousel from "react-material-ui-carousel";
+
+import RoomOutlinedIcon from "@material-ui/icons/RoomOutlined";
+import RoomRoundedIcon from "@material-ui/icons/RoomRounded";
+import BusinessOutlinedIcon from "@material-ui/icons/BusinessOutlined";
 
 import GoogleMapReact from "google-map-react";
 
@@ -89,6 +95,7 @@ export default function BizList(props) {
   const [map, setMap] = useState(null);
   const [totalPages, setTotalPages] = useState(1);
   const [bizList, setBizList] = useState([]);
+  const [hover, setHover] = useState(null);
 
   const fetchNewList = page => {
     let category = values.c;
@@ -122,6 +129,14 @@ export default function BizList(props) {
     fetchNewList(1);
   }, [props.location.search]);
 
+  const handleMouseEnter = idx => e => {
+    setHover(idx);
+  };
+
+  const handleMouseLeave = idx => e => {
+    setHover(null);
+  };
+
   if (bizList.length === 0) return null;
 
   return (
@@ -137,85 +152,141 @@ export default function BizList(props) {
             <Grid
               container
               spacing={4}
+              onMouseEnter={handleMouseEnter(idx)}
+              onMouseLeave={handleMouseLeave(idx)}
               onClick={() => props.history.push(`/biz/${biz._id}`)}
             >
+              {/* <Grid item xs={2}>
+                <Carousel>
+                  {biz.photos.map(photo => (
+                    <CardMedia
+                      style={{ width: 200, height: 200 }}
+                      image={
+                        process.env.PUBLIC_URL + `/photos/${photo._id}.jpg`
+                      }
+                    />
+                  ))}
+                </Carousel>
+              </Grid> */}
+
               <Grid item xs={12}>
                 <Card variant="outlined" style={{ background: "#f5f5f5" }}>
                   <CardActionArea>
                     <CardContent>
-                      <Box display="flex">
-                        <Box flexGrow={1}>
-                          <Typography variant="h5" style={{ fontWeight: 700 }}>
-                            {`${pagingCounter + idx}. ${biz.name}`}
-                          </Typography>
-                        </Box>
+                      <Box display="flex" flexDirection="row">
                         <Box>
-                          <Typography variant="caption">
-                            {biz.address}
-                          </Typography>
-                        </Box>
-                      </Box>
-
-                      <Box borderColor="transparent">
-                        <div className={classes.ratingRoot}>
-                          <Rating
-                            name="read-only"
-                            value={biz.stars}
-                            precision={0.5}
-                            readOnly
-                          />
-                          <Box ml={2}>
-                            <Typography component="legend">
-                              {biz.review_count} reviews
-                            </Typography>
-                          </Box>
-                        </div>
-                      </Box>
-
-                      <Box borderColor="transparent">
-                        <div className={classes.categoriesRoot}>
-                          {biz.attributes &&
-                          biz.attributes.RestaurantsPriceRange2 ? (
-                            <Typography variant="subtitle1" gutterBottom>
-                              {dollarSign(
-                                biz.attributes.RestaurantsPriceRange2
-                              )}
-                              {bull}
-                            </Typography>
-                          ) : (
-                            ""
-                          )}
-                          <Typography variant="subtitle2" gutterBottom>
-                            {biz.categories
-                              .slice(0, 2)
-                              .map(category => (
-                                // <Link
-                                //   key={category}
-                                //   href={`/#/c/${biz.city.toLowerCase()}/${category.toLowerCase()}`}
-                                // >
-                                <Link
-                                  key={category}
-                                  underline="none"
-                                  component={RouterLink}
-                                  to={
-                                    "/search?c=" +
-                                    encodeURIComponent(category) +
-                                    "&loc=" +
-                                    encodeURIComponent(`${city},${state}`)
+                          {biz.photos.length > 0 ? (
+                            <Carousel
+                              autoPlay={false}
+                              indicators={false}
+                              // animation="slide"
+                            >
+                              {biz.photos.map(photo => (
+                                <CardMedia
+                                  style={{
+                                    width: 150,
+                                    height: 150,
+                                    borderRadius: 5,
+                                    marginRight: 16
+                                  }}
+                                  image={
+                                    process.env.PUBLIC_URL +
+                                    `/photos/${photo._id}.jpg`
                                   }
-                                  onClick={handleCategoryClick(
-                                    "/search?c=" +
-                                      encodeURIComponent(category) +
-                                      "&loc=" +
-                                      encodeURIComponent(`${city},${state}`)
+                                />
+                              ))}
+                            </Carousel>
+                          ) : (
+                            <BusinessOutlinedIcon
+                              style={{
+                                width: 150,
+                                height: 150,
+                                borderRadius: 5,
+                                marginRight: 16
+                              }}
+                            />
+                          )}
+                        </Box>
+
+                        <Box flexGrow={1}>
+                          <Box display="flex">
+                            <Box flexGrow={1}>
+                              <Typography
+                                variant="h5"
+                                style={{ fontWeight: 700 }}
+                              >
+                                {`${pagingCounter + idx}. ${biz.name}`}
+                              </Typography>
+                            </Box>
+                            <Box>
+                              <Typography variant="caption">
+                                {biz.address}
+                              </Typography>
+                            </Box>
+                          </Box>
+
+                          <Box borderColor="transparent">
+                            <div className={classes.ratingRoot}>
+                              <Rating
+                                name="read-only"
+                                value={biz.stars}
+                                precision={0.5}
+                                readOnly
+                              />
+                              <Box ml={2}>
+                                <Typography component="legend">
+                                  {biz.review_count} reviews
+                                </Typography>
+                              </Box>
+                            </div>
+                          </Box>
+
+                          <Box borderColor="transparent">
+                            <div className={classes.categoriesRoot}>
+                              {biz.attributes &&
+                              biz.attributes.RestaurantsPriceRange2 ? (
+                                <Typography variant="subtitle1" gutterBottom>
+                                  {dollarSign(
+                                    biz.attributes.RestaurantsPriceRange2
                                   )}
-                                >
-                                  {category}
-                                </Link>
-                              ))
-                              .reduce((prev, curr) => [prev, ", ", curr])}
-                          </Typography>
-                        </div>
+                                  {bull}
+                                </Typography>
+                              ) : (
+                                ""
+                              )}
+                              <Typography variant="subtitle2" gutterBottom>
+                                {biz.categories
+                                  .slice(0, 2)
+                                  .map(category => (
+                                    // <Link
+                                    //   key={category}
+                                    //   href={`/#/c/${biz.city.toLowerCase()}/${category.toLowerCase()}`}
+                                    // >
+                                    <Link
+                                      key={category}
+                                      underline="none"
+                                      component={RouterLink}
+                                      to={
+                                        "/search?c=" +
+                                        encodeURIComponent(category) +
+                                        "&loc=" +
+                                        encodeURIComponent(`${city},${state}`)
+                                      }
+                                      onClick={handleCategoryClick(
+                                        "/search?c=" +
+                                          encodeURIComponent(category) +
+                                          "&loc=" +
+                                          encodeURIComponent(`${city},${state}`)
+                                      )}
+                                    >
+                                      {category}
+                                    </Link>
+                                  ))
+                                  .reduce((prev, curr) => [prev, ", ", curr])}
+                              </Typography>
+                            </div>
+                          </Box>
+                        </Box>
                       </Box>
                     </CardContent>
                   </CardActionArea>
@@ -249,14 +320,29 @@ export default function BizList(props) {
           // options={createMapOptions}
           onChange={handleMapChange}
         >
-          {bizList.map(biz => (
-            <AnyReactComponent
-              key={biz._id}
-              lat={biz.latitude}
-              lng={biz.longitude}
-              text="https://yelp-images.s3.amazonaws.com/assets/map-markers/annotation_32x43.png"
-            />
-          ))}
+          {bizList.map((biz, idx) =>
+            // <AnyReactComponent
+            //   key={biz._id}
+            //   lat={biz.latitude}
+            //   lng={biz.longitude}
+            //   text="https://yelp-images.s3.amazonaws.com/assets/map-markers/annotation_32x43.png"
+            // />
+            hover == idx ? (
+              <RoomOutlinedIcon
+                key={biz._id}
+                lat={biz.latitude}
+                lng={biz.longitude}
+                fontSize="large"
+              />
+            ) : (
+              <RoomRoundedIcon
+                key={biz._id}
+                lat={biz.latitude}
+                lng={biz.longitude}
+                fontSize="large"
+              />
+            )
+          )}
         </GoogleMapReact>
       </div>
     </div>
